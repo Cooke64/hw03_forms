cb8efa550c950a1cfa5f7fe17da03a46f3d5699b
cooke09 <pozdeev199@mail.ru>
2022-01-27 08:46:54 +0300

    post_detail

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -56,6 +56,7 @@ def group_posts(request, slug):
     }
     return render(request, template, context)
 
+
 # Исправлено в шаблоне post_detail
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
